94fa6439d14eb97dc130727b0763b74ece96b83a
Vladimir Borisov <1mt-2@mail.ru>
2022-11-05 19:42:21 +0300

    added use-hook-form and material UI

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,6 +1,10 @@
 import React from "react";
 import s from './Contacts.module.scss'
 import styleContainer from "../Common/Styles/Container.module.css";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {TextField} from "@mui/material";
+
+
 
 function Contacts() {
     return (
@@ -8,16 +12,46 @@ function Contacts() {
             <div className={`${styleContainer.container} ${s.contactsContainer}`} >
 
                     <h3>Contacts</h3>
-                    <form className={s.form}>
-                        <input type={'text'}/>
-                        <input type={'text'}/>
-                        <textarea/>
-                    </form>
-                    <button type={'submit'} className={s.button}>Send</button>
-
+                    {/*<form className={s.form}>*/}
+                    {/*    <input type={'text'}/>*/}
+                    {/*    <input type={'text'}/>*/}
+                    {/*    <textarea/>*/}
+                    {/*</form>*/}
+                    {/*<button type={'submit'} className={s.button}>Send</button>*/}
+                    <ContactForm />
             </div>
         </div>
     );
 }
 
 export default Contacts;
+
+
+
+const ContactForm = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return (
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+            {/*<input {...register("email")} />*/}
+            {/*<input {...register("number")} />*/}
+            {/*<textarea {...register("message")} />*/}
+
+            <TextField className={s.textField} id="standard-basic" label="Email" variant="standard" {...register("email")} />
+            <TextField id="standard-basic" label="Number" variant="standard" {...register("number")} />
+
+            <TextField
+                id="standard-multiline-static"
+                label="Your message"
+                multiline
+                rows={4}
+                // defaultValue="Default Value"
+                variant="standard"
+                {...register("message")}
+            />
+
+            <input className={s.button}  type="submit" />
+        </form>
+    );
+}
