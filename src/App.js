528428d35530100aba2facef1c553d53b51514a8
Vladimir Borisov <1mt-2@mail.ru>
2022-11-16 19:07:51 +0300

    style: minor css changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import './App.scss';
 import Header from "./Header/Header";
 import MainBlock from "./MainBlock/MainBlock";
 import Skills from "./Skills/Skills";
